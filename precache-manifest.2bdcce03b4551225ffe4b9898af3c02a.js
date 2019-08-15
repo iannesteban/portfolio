930e752084e3f77ceeebe681911a7fa53064abf3
Iann Esteban <ciannesteban@gmail.com>
2019-08-15 23:54:00 +0400

    first commit

---
+++ precache-manifest.2bdcce03b4551225ffe4b9898af3c02a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888741bd558b31e2b477ddab0cd5f5f9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "acb3c710549c8d728cc7",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "13595c60d6721c15afe8",
+    "url": "/portfolio/static/js/2.4126415c.chunk.js"
+  },
+  {
+    "revision": "acb3c710549c8d728cc7",
+    "url": "/portfolio/static/js/main.e9bce766.chunk.js"
+  },
+  {
+    "revision": "e5d934b965fecb1758fe",
+    "url": "/portfolio/static/js/runtime~main.2d8a99c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
